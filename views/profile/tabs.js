1fc55cac3165a90045be211e856a9ea75571eb6b
Rizky <rizky05@gmail.com>
2012-12-05 13:56:33 +0700

    Fixing tabs.js behaviour

--- views/profile/tabs.js
+++ views/profile/tabs.js
@@ -1,13 +1,16 @@
 
 $(function() {
     $tab = window.location.hash;
-    if ($tab != "") {
-        $('.tabs-nav li.active').removeClass('active');
-        $('.tabs-content.active').removeClass('active');
+	$('.tabs-nav li.active').removeClass('active');
+	$('.tabs-content.active').removeClass('active');
 	
+    if ($tab != "") {
         $("a[href="+$tab+"]").parent().addClass('active');
         $($tab).addClass('active');
-    }
+    } else if ($("a[href="+$tab+"]").length  == 0 || $tab == "") {
+        $(".tabs-nav li:eq(0) a").click();
+	}
+	
     $('.tabs-nav li a').click(function() {
         $('.tabs-nav li.active').removeClass('active');
         $('.tabs-content.active').removeClass('active');
