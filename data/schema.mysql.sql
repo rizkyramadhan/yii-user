fb3a7a9712034b700dbe0b0d48e5cb6852f44b44
Rizky <rizky05@gmail.com>
2012-12-05 13:50:05 +0700

    Fixing initial .sql, removing migration

--- data/schema.mysql.sql
+++ data/schema.mysql.sql
@@ -1,60 +1,64 @@
-CREATE TABLE `tbl_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `activkey` varchar(128) NOT NULL DEFAULT '',
-  `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastvisit` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `superuser` int(1) NOT NULL DEFAULT '0',
-  `status` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
-  KEY `status` (`status`),
-  KEY `superuser` (`superuser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+-- Adminer 3.6.1 MySQL dump
 
-CREATE TABLE `tbl_profiles` (
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE `profiles` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(50) NOT NULL DEFAULT '',
-  `firstname` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `user_profile_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `tbl_profiles`
-  ADD CONSTRAINT `user_profile_id` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`id`) ON DELETE CASCADE;
+INSERT INTO `profiles` (`user_id`) VALUES
+(1);
 
-CREATE TABLE `tbl_profiles_fields` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `varname` varchar(50) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `field_type` varchar(50) NOT NULL,
-  `field_size` varchar(15) NOT NULL DEFAULT '0',
-  `field_size_min` varchar(15) NOT NULL DEFAULT '0',
+DROP TABLE IF EXISTS `profiles_fields`;
+CREATE TABLE `profiles_fields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `varname` varchar(50) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `field_type` varchar(50) NOT NULL DEFAULT '',
+  `field_size` int(3) NOT NULL DEFAULT '0',
+  `field_size_min` int(3) NOT NULL DEFAULT '0',
   `required` int(1) NOT NULL DEFAULT '0',
   `match` varchar(255) NOT NULL DEFAULT '',
   `range` varchar(255) NOT NULL DEFAULT '',
   `error_message` varchar(255) NOT NULL DEFAULT '',
-  `other_validator` varchar(5000) NOT NULL DEFAULT '',
+  `group` varchar(25) NOT NULL,
+  `subgroup` varchar(25) NOT NULL,
+  `other_validator` text,
   `default` varchar(255) NOT NULL DEFAULT '',
   `widget` varchar(255) NOT NULL DEFAULT '',
-  `widgetparams` varchar(5000) NOT NULL DEFAULT '',
+  `widgetparams` text,
   `position` int(3) NOT NULL DEFAULT '0',
   `visible` int(1) NOT NULL DEFAULT '0',
+  `visible_for_role` varchar(255) NOT NULL,
+  `column_number` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL DEFAULT '',
+  `activkey` varchar(128) NOT NULL DEFAULT '',
+  `superuser` int(1) NOT NULL DEFAULT '0',
+  `role` varchar(10) NOT NULL DEFAULT 'user',
+  `status` int(1) NOT NULL DEFAULT '0',
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastvisit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
-  KEY `varname` (`varname`,`widget`,`visible`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  UNIQUE KEY `user_username` (`username`),
+  UNIQUE KEY `user_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `activkey`, `superuser`, `role`, `status`, `create_at`, `lastvisit_at`) VALUES
+(1,	'admin',	'21232f297a57a5a743894a0e4a801fc3',	'admin@admin.com',	'2ec9af216fca40118816ad0feac8af29',	1,	'pengelola',	1,	'2012-11-29 13:35:35',	'2012-11-30 19:01:17');
 
-INSERT INTO `tbl_users` (`id`, `username`, `password`, `email`, `activkey`, `superuser`, `status`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'webmaster@example.com', '9a24eff8c15a6a141ece27eb6947da0f', 1, 1),
-(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', 'demo@example.com', '099f825543f7850cc038b90aaff39fac', 0, 1);
-
-INSERT INTO `tbl_profiles` (`user_id`, `lastname`, `firstname`) VALUES
-(1, 'Admin', 'Administrator'),
-(2, 'Demo', 'Demo');
-
-INSERT INTO `tbl_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
-(1, 'lastname', 'Last Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect Last Name (length between 3 and 50 characters).', '', '', '', '', 1, 3),
-(2, 'firstname', 'First Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect First Name (length between 3 and 50 characters).', '', '', '', '', 0, 3);
+-- 2012-12-05 13:49:11
